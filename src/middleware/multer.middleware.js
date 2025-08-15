a07aafe3a2dda02f7eb1e77e8365be0983cb7511
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-08-16 04:02:10 +0530

    multer issue

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,4 +1,13 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadPath = path.join(process.cwd(), "public", "temp");
+
+// Create the folder if it doesn't exist
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage= multer.diskStorage({
     destination:function(req,file,cb){
